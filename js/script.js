c394258a12470d5134b7b9e0dd699a807669e344
zizEast <jonnyashghere96@gmail.com>
2022-12-14 18:16:29 +0100

    inserimento prime pagine per prova velocità
    chisiamo-index-servizi-vetrofanie

---
+++ js/script.js
@@ -0,0 +1,76 @@
+/*  -----------------------------------------------------------------------------------------------
+  header fixed
+--------------------------------------------------------------------------------------------------- */
+const header =document.querySelector('.header')
+console.log(header);
+
+document.addEventListener('scroll' , () => {
+    console.log('ok');
+    if(document.documentElement.scrollTop > 0) {
+        console.log('ok');
+        header.classList.add('--fixed')
+    } else {
+        header.classList.remove('--fixed')
+    }
+})
+
+
+/*  -----------------------------------------------------------------------------------------------
+  menu active
+--------------------------------------------------------------------------------------------------- */
+
+const btnMenu = document.querySelector ('.icon')
+const navList = document.querySelector ('.header .nav-list')
+
+const aTag=document.querySelectorAll('a')
+
+
+btnMenu.addEventListener ('click' , () => {
+    btnMenu.classList.toggle ('--active')
+    navList.classList.toggle('--active')
+    document.body.classList.toggle('noScroll')
+})
+
+
+
+/*  -----------------------------------------------------------------------------------------------
+  scroll event
+--------------------------------------------------------------------------------------------------- */
+
+
+function revealFunction1 () {
+    window.sr = ScrollReveal({duration:700 , distance:'200px',easing:'ease-out',opacity:0})
+    sr.reveal('.reveal-left', {origin:'left',reset:true})
+    sr.reveal('.reveal-right', {origin:'right',reset:true})
+    sr.reveal('.reveal-right-delay', {origin:'right',reset:true, delay:400})
+    sr.reveal('.reveal-bottom', {origin:'bottom',reset:true, delay:400})
+    sr.reveal('.reveal-top', {origin:'top',reset:true,delay:400})
+
+    sr.reveal('.rotate', {origin:'bottom' ,rotate:{x:1000,z:1000}}) //rotazione
+    sr.reveal('.spot-light', {distance:'-80px', opacity:0 ,reset:true,duration:600})  // opacità
+    sr.reveal('.scale-up', {distance:'0px', scale:1})
+}
+
+
+
+window.addEventListener('load', () => {
+    revealFunction1()
+})
+
+/*  -----------------------------------------------------------------------------------------------
+  soluzioni btn
+--------------------------------------------------------------------------------------------------- */
+
+const solutionBtns = document.querySelectorAll ('.solution-w button')
+
+solutionBtns.forEach(btn => {
+    btn.addEventListener('click' , () => {
+        btn.classList.toggle('--active')
+    })
+})
+
+
+
+
+
+
